a0b50816a44ed64170489b9e37e4e66c73fb2ab7
xuehai <xhzeng@thoughtworks.com>
2012-08-15 17:57:01 +0800

    added some support for UIAtomation test

--- Frank/features/support/env.rb
+++ Frank/features/support/env.rb
@@ -5,6 +5,5 @@ Frank::Cucumber::FrankHelper.use_shelley_from_now_on
 
 # TODO: set this constant to the full path for your Frankified target's app bundle.
 # See the "Given I launch the app" step definition in launch_steps.rb for more details
-#APP_BUNDLE_PATH = nil
 
 APP_BUNDLE_PATH = File.expand_path( '../../../frankified_build/Frankified.app', __FILE__ )
