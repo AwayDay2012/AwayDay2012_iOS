70524db187851931ce9651a2a119275e657c75b5
xuehai <xhzeng@thoughtworks.com>
2012-09-21 16:07:32 +0800

    Edited the test configuration

---
+++ AwayDay2012Test/AwayDay2012Test.h
@@ -0,0 +1,13 @@
+//
+//  AwayDay2012Test.h
+//  AwayDay2012Test
+//
+//  Created by xuehai on 9/21/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AwayDay2012Test : SenTestCase
+
+@end
---
+++ AwayDay2012Test/AwayDay2012Test.m
@@ -0,0 +1,28 @@
+//
+//  AwayDay2012Test.m
+//  AwayDay2012Test
+//
+//  Created by xuehai on 9/21/12.
+//
+//
+
+#import "AwayDay2012Test.h"
+
+@implementation AwayDay2012Test
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+
+@end
